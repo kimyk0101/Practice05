c1cd86620f5eb4e5c5f6bc31e1c5f59e46b3f944
kimyk0101 <kimyk7131@gmail.com>
2024-11-08 13:00:41 +0900

    Practice 7번문제

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -14,14 +14,28 @@ public class FriendApp {
         
         //for문 시작
         //친구정보 3명 입력 로직 --> 반복문 사용
-            
-      
-        //for문 끝
-        
+        for (int i = 0; i < friendArray.length; ++i) {
+        	String name, hp, school;
+        	
+        	System.out.print("이름:");
+        	name = sc.next();
+        	System.out.print("핸드폰:");
+        	hp = sc.next();
+        	System.out.print("학교:");
+        	school = sc.next();
+        	
+        	Friend f = new Friend(name, hp, school);
+        	friendArray[i] = f;
+        	
+        	System.out.println("------------------");
+        	//for문 끝
+        }
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
             //친구정보 출력 메소드 호출
+        	Friend f = friendArray[i];
+        	f.showInfo();
         }
 
         sc.close();
