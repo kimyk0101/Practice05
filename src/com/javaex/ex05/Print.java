16e6a8085cfc08977d5c5bbfcc28863249dc0cb4
kimyk0101 <kimyk7131@gmail.com>
2024-11-14 18:07:33 +0900

    5번 문제

--- src/com/javaex/ex05/Print.java
+++ src/com/javaex/ex05/Print.java
@@ -5,8 +5,16 @@ public class Print {
     public void printer(int val){
         System.out.println(val);
     }
-    
-     
     //메소드  3개 를 작성하세요
-    //
+    public void printer(boolean b) {
+    	System.out.println(b);
+    }
+    
+    public void printer(double d) {
+    	System.out.println(d);
+    }
+    
+    public void printer(String name) {
+    	System.out.println(name);
+    }
 }
