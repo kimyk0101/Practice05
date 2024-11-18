5f86350af570f56da01dd5666a951c1f78a405b8
kimyk0101 <kimyk7131@gmail.com>
2024-11-18 17:26:16 +0900

    문제 9번

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,10 +2,23 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
-    public static String concatString(){
-       
-        //메소드 내용작성
+    public static String concatString(String[] strArray){
+    	String result = "";
         
+    	/*방법1. 기본적인 루프
+    	for (int i = 0; i < strArray.length; ++i) {
+    		result += strArray[i];
+    	}*/
+    	
+    	/* 방법2. 향상된 For 루프
+    	for (String item: strArray) {
+    		result += item;
+    	}*/
+    	
+    	/* 방법3. 문자열 배열 합치기*/
+    	result = String.join("", strArray);
+    	
+        return result;
     }
 
 }
