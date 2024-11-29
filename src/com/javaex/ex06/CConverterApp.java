f874d833fcacb8acdda2d3334a6486b4087a3c6d
kimyk0101 <kimyk7131@gmail.com>
2024-11-29 18:17:03 +0900

    6번 문제 완료, 10번 문제 진행중

--- src/com/javaex/ex06/CConverterApp.java
+++ src/com/javaex/ex06/CConverterApp.java
@@ -11,10 +11,10 @@ public class CConverterApp {
         CConverter.setRate(1118.70);
         
         //100만원을 달러로 출력하기
-        System.out.println("백만원은 " + toDollar(won) + "달러입니다.");
+        System.out.println("백만원은 " + 	CConverter.toDollar(1000000) + "달러입니다.");
         
         //100달려를 원으로 출력하기
-        System.out.println("백만원은 " + toKWR(dollar) + "달러입니다.");
+        System.out.println("백달러는 " + CConverter.toKWR(100) + "원입니다.");
         
     }
 
--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,9 +1,45 @@
 package com.javaex.ex10;
 
 public class Book {
+    private int bookNo;
+    private String title;
+    private String author;
+    private int stateCod;
     
+	public int getBookNo() {
+		return bookNo;
+	}
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getStateCod() {
+		return stateCod;
+	}
+	public void setStateCod(int stateCod) {
+		this.stateCod = stateCod;
+	}
+	
+	public Book(int bookNo, String title, String author, int stateCod) {
+		super();
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+		this.stateCod = stateCod;
+	}
     
+    public static void rent() {}
     
-    
-    
+    public static void print() {}
 }
